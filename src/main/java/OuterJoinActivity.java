d1629b62ff20c6e251cb26fb9d466f690ba0be43
grantxtaylor <xgrtaylor@gmail.com>
2023-03-01 19:53:21 +0000

    complete

--- src/main/java/OuterJoinActivity.java
+++ src/main/java/OuterJoinActivity.java
@@ -118,7 +118,7 @@ public class OuterJoinActivity {
     public Set<ExampleEntity> problem2() {
         /**
          * Problem 2: In problem2.sql, Use a RIGHT OUTER JOIN to combine the class (left side) and textbook
-         * (right side) tables using the class_title column as the join on column. This means that when no class
+         * (right side) tables usidng the class_title column as the join on column. This means that when no class
          * exists for a textbook, a row with a NULL class will be in the resulting query.
          * Hint: You should start with "SELECT * FROM class"
          */
